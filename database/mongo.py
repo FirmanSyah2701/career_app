fae02afa118c04909b376ccfe0e3cfea1eca4e81
Firman Syah <abdulfirmansyah7@gmail.com>
2022-07-17 09:22:44 +0700

    coba fix 3

--- database/mongo.py
+++ database/mongo.py
@@ -1,6 +1,6 @@
-import motor.motor_asyncio
 from core.config import settings
+""" import motor.motor_asyncio
 
 client = motor.motor_asyncio.AsyncIOMotorClient(settings.MONGODB_URL)
 
-database = client.career_students
+database = client.career_students """ 
