b3b08897e0edf7f193f140b13216d90a2f08ae3c
Firman Syah <abdulfirmansyah7@gmail.com>
2022-07-19 00:21:33 +0700

    fix form multipart

--- main.py
+++ main.py
@@ -1,24 +1,9 @@
 from fastapi import FastAPI
-""" from fastapi.middleware.cors import CORSMiddleware """
 from fastapi.staticfiles import StaticFiles
 from routers import admin, career, auth, web
-from database import migration 
 from core.config import templates
 
 app = FastAPI()
-
-""" origins = [
-    "http://127.0.0.1:8000",
-    "http://localhost:8000"
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-) """
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
@@ -26,7 +11,6 @@ app.include_router(admin.router)
 app.include_router(career.router)
 app.include_router(auth.router)
 app.include_router(web.router)
-app.include_router(migration.router)
 
 @app.exception_handler(404)
 async def custom_404_handler(request, __):
